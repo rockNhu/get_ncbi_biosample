d82bedd317cdbf271553b02267c39646208fb34a
Rock Nhu <nhuhuganaxis@outlook.com>
2022-11-28 22:24:47 +0800

    update args

--- get_ncbi_info.py
+++ get_ncbi_info.py
@@ -1,79 +1,91 @@
 import pandas
-import re,os
+import re
+import os
+import pathlib
 import requests
+import argparse
+
+
 class get_info(object):
     '''This class is for catching biosample's attribute.'''
+
     def __init__(self):
+        self.args = self.args_parser()
         self.simplify()
         self.main()
-        self.input_gate = 'net'
-        self.na_ph = self.get_input()
         self.refined()
-    
-    def get_input(self):
+
+    def args_parser(self):
+        parser = argparse.ArgumentParser(
+            description='This script is used to get biosamples in NCBI.')
+        parser.add_argument('-i', '--input', required=True,
+            help='Path of input tsv,\
+            the ncbi_table must include "Assembly Accession" and\
+            "Assembly BioSample Accession" columns.'
+            )
+        parser.add_argument('-o', '--output', required=True,
+            help='Path to output.'
+            )
+        return parser.parse_args()
+
+    def get_input(self, gate):
+        '''List the xml dir.'''
         na_ph = []
-        for file in os.listdir(self.input_gate):
-            name = os.path.basename(file)[:-4]
-            path = os.path.join(self.input_gate,file)
-            na_ph.append([name,path])
+        for file in os.listdir(gate):
+            name = os.path.basename(file).rsplit('.', 1)[0]
+            path = os.path.join(gate, file)
+            na_ph.append([name, path])
         return na_ph
 
     def simplify(self):
-        df = pandas.read_csv('prokaryotes.csv')
+        '''The table is download from NCBI, and just need 2 columns now.'''
+        df = pandas.read_csv(self.args.input, sep='\t')
         new = pandas.DataFrame()
-        new['Strain'] = df['Strain']
-        new['BioSample'] = df['BioSample']
-        new['Level'] = df['Level']
-        new['GenBank FTP'] = df['GenBank FTP']
-        new.to_csv('Kv.tsv',sep='\t',index=False)
+        new['Strain'] = df['Assembly Accession']
+        new['BioSample'] = df['Assembly BioSample Accession']
+        new.to_csv('temp.tsv', sep='\t', index=False)
 
     def main(self):
+        '''Get the biosample and save to xml.'''
+        if not os.path.exists('net'):
+            os.mkdir('net')
         head_num = 1
-        for line in open('Kv.tsv'):
+        for line in open('temp.tsv'):
             if head_num:
                 head_num -= 1
                 continue
-            strain,biosample = line.split('\t')[0:2]
-            url = 'https://www.ncbi.nlm.nih.gov/biosample/{}/'.format(biosample)
-            output_name = 'net/{}.xml'.format(strain.replace("/",'_-').replace(':',"--"))
-            if os.path.exists(output_name) and os.stat(output_name).st_size != 0:
+            strain, biosample = line.strip().split('\t')
+            if os.path.exists(f'net/{strain}.xml'):
                 continue
-            try:
-                r = requests.get(url)
-            except:
-                print('error get : {}'.format(strain))
-            try:
-                with open(output_name,'w') as f:
-                    f.write(r.text)
-            except:
-                print('error write : {}'.format(strain))
-        
+            url = f'https://www.ncbi.nlm.nih.gov/biosample/{biosample}/'
+            r = requests.get(url)
+            with open(f'net/{strain}.xml', 'w') as f:
+                f.write(r.text)
+
     def refined(self):
+        '''The final filter, get the annotation table.'''
+        if not os.path.exists('temp'):
+            os.mkdir('temp')
         total = []
-        for bsname,path in self.na_ph:
-            sample = {}
-            with open(path) as f:
-                content = f.read()
-            record = re.findall('<tr><th>(.*?)</th><td>(.*?)</td></tr>',content)
-            for rec in record:
-                sample.update({rec[0]:rec[1]})
-            total.append([bsname,sample])
-        for bs,s in total:
+        for bsname, path in self.get_input('net'):
+            content = pathlib.Path(path).read_text()
+            record = re.findall(
+                '<tr><th>(.*?)</th><td>(.*?)</td></tr>', content)
+            sample = {rec[0]: rec[1] for rec in record}
+            total.append([bsname, sample])
+        for bs, s in total:
             sdf = pandas.DataFrame([s])
-            sdf.to_csv('temp/{}.tsv'.format(bs),sep='\t',index=False)
+            sdf.to_csv(f'temp/{bs}.tsv', sep='\t', index=False)
         i = 1
         for file in os.listdir('temp'):
-            if i:
-                df1 = pandas.read_csv('temp/{}'.format(file),sep='\t').astype(str)
+            if i:  # the first line
+                df1 = pandas.read_csv(f'temp/{file}', sep='\t').astype(str)
                 i -= 1
                 continue
-            try:
-                df2 = pandas.read_csv('temp/{}'.format(file),sep='\t').astype(str)
-                df1 = pandas.merge(df1,df2,how='outer')
-            except:
-                print('error tsv : {}'.format(file))
-        df1.to_csv('kv_output.tsv',sep='\t',index=False)
+            df2 = pandas.read_csv(f'temp/{file}', sep='\t').astype(str)
+            df1 = pandas.merge(df1, df2, how='outer')
+        df1.to_csv(self.args.output, sep='\t', index=False)
+
 
 if __name__ == '__main__':
     get_info()
-
